addc5bf77e1c590512e629c546abeadd25e71d7f
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2018-12-30 23:48:51 -0500

    idk

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -4,10 +4,11 @@ class RoomsChannel < ApplicationCable::Channel
     stream_from "room_#{params[:room_id]}"
     user_id = params[:user_id].to_i
     room_id = params[:room_id].to_i
-    user_room = UserRoom.create(user_id: user_id, room_id: room_id)
     room = Room.find(room_id)
+    user_room = UserRoom.create(user_id: user_id, room_id: room_id)
     ActionCable.server.broadcast "room_#{user_room.room_id}", { title: 'User joined', body: User.find(user_room.user_id) }
     ActionCable.server.broadcast "room_#{room_id}", { title: 'All users', body: room.users }
+
   end
 
   def receive(data)
@@ -21,6 +22,7 @@ class RoomsChannel < ApplicationCable::Channel
     delete_room = UserRoom.find_by(user_id: user_id, room_id: room_id)
     UserRoom.delete(delete_room)
     ActionCable.server.broadcast "room_#{room_id}", { title: 'All users', body: room.users }
+    ActionCable.server.broadcast "room_#{room_id}", { title: 'host_change', body: room.users }
   end
 
 end
