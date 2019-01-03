eda6a37a5c6af7829502ff1a9648939eeceab99b
Trinacria <xcstolfax@gmail.comgit config --global user.name Christopher Stolfagit config --global user.email xcstolfax@gmail.com>
2019-01-02 21:18:39 -0500

    fixing the security issue broke my app, reverted back to previous form

--- app/channels/rooms_channel.rb
+++ app/channels/rooms_channel.rb
@@ -6,11 +6,11 @@ class RoomsChannel < ApplicationCable::Channel
     room_id = params[:room_id].to_i
     room = Room.find(room_id)
     user_room = UserRoom.create(user_id: user_id, room_id: room_id)
-    room_users = room.users.map do |user|
-      { id: user.id, bio: user.bio, username: user.username, avatar: user.avatar }
-    end
+    # room_users = room.users.map do |user|
+    #   { id: user.id, bio: user.bio, username: user.username, avatar: user.avatar }
+    # end
     ActionCable.server.broadcast "room_#{user_room.room_id}", { title: 'User joined', body: User.find(user_room.user_id) }
-    ActionCable.server.broadcast "room_#{room_id}", { title: 'All users', body: { users: room_users, videos: room.youtubes, messages: room.messages } }
+    ActionCable.server.broadcast "room_#{room_id}", { title: 'All users', body: { users: room.users, videos: room.youtubes, messages: room.messages } }
 
   end
 
@@ -22,13 +22,13 @@ class RoomsChannel < ApplicationCable::Channel
     user_id = params[:user_id].to_i
     room_id = params[:room_id].to_i
     room = Room.find(room_id)
-    room_users = room.users.map do |user|
-      { id: user.id, bio: user.bio, username: user.username, avatar: user.avatar }
-    end
+    # room_users = room.users.map do |user|
+    #   { id: user.id, bio: user.bio, username: user.username, avatar: user.avatar }
+    # end
     delete_room = UserRoom.find_by(user_id: user_id, room_id: room_id)
     UserRoom.delete(delete_room)
     # ActionCable.server.broadcast "room_#{room_id}", { title: 'All users', body: room.users }
-    ActionCable.server.broadcast "room_#{room_id}", { title: 'host_change', body: room_users }
+    ActionCable.server.broadcast "room_#{room_id}", { title: 'host_change', body: room.users }
   end
 
 end
