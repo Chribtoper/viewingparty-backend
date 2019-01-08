0180a9cc15a9d54c4c4d73b62cbfcca2e41f86e1
Chribtoper <xcstolfax@gmail.com>
2019-01-08 14:41:34 -0500

    changed products vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
   # config.web_socket_server_url = "wss://viewingparty-app.herokuapp.com/cable"
-  # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
-  config.action_cable.allowed_request_origins = ["https://viewing-party-app.herokuapp.com/", "https://viewing-party-app.herokuapp.com/"]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  # config.action_cable.allowed_request_origins = ["https://viewing-party-app.herokuapp.com/", "https://viewing-party-app.herokuapp.com/"]
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
