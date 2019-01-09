acd06854b8fe19d6b40dbcd2d29dbeeaa0264719
Chribtoper <xcstolfax@gmail.com>
2019-01-09 18:37:14 -0500

    debug attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,13 @@ Rails.application.configure do
   config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
   config.web_socket_server_url = "wss://#{ENV['RAILS_HOST']}/cable"
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
-  $redis = if Rails.env.production? ? Redis.new(ENV["REDIS_URL"]) : Redis.new("redis://localhost:6379")
+  if Rails.env.production?
+  puts "production: #{ENV['REDIS_URL']}"
+  uri = URI.parse(ENV["REDIS_URL"])
+else
+  puts "not production"
+  uri = URI.parse("redis://localhost:6379")
+end
   # config.action_cable.allowed_request_origins = ["https://viewingparty-app.herokuapp.com/", /https:\/\/viewingparty-app.herokuapp.*/]
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
